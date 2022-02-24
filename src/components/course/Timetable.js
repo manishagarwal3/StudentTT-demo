c720032d7625da3b87dbe121eef1b296fcb5dda9
manishagarwal3 <manish.agarwal@thoughtworks.com>
2022-02-24 22:35:36 +0530

    a

--- src/components/course/Timetable.js
+++ src/components/course/Timetable.js
@@ -7,3 +7,13 @@ const Timetable = () => {
 }
  
 export default Timetable;
+
+const Timetable = () => {
+    return ( 
+        <div>
+            <h2>Timetable</h2>
+        </div>
+     );
+}
+ 
+export default Timetable;
